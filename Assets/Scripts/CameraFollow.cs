4bd5804cbd4e60c9de1d9f6ba60b1310b1d304fd
Zachary M Thomas <zach711595@hotmail.com>
2021-08-30 18:27:29 -0400

    change camera update

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,10 +6,15 @@ public class CameraFollow : MonoBehaviour
 {
 
     public GameObject player;
-    public int cameraBackPosition = 500;
+    public int cameraBackPosition = 10;
 
     void LateUpdate()
     {
-        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(0, gameObject.transform.position.y, player.transform.position.z - cameraBackPosition), Time.deltaTime * 100);
+        transform.position = new Vector3(0, gameObject.transform.position.y, player.gameObject.transform.position.z - cameraFollowDistance);
     }
+
+    //void LateUpdate()
+    //{
+    //    gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(0, gameObject.transform.position.y, player.transform.position.z - cameraBackPosition), Time.deltaTime * 100);
+    //}
 }
