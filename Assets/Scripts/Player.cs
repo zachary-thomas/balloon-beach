b91f2c54fd8bea2a5b961b2eec56f1dcd90da7c0
Zachary M Thomas <zach711595@hotmail.com>
2021-09-22 17:48:22 -0400

    add audio to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
 
     public float playerSpeed = 1500;
     public float directionalSpeed = 20;
+    public AudioClip scoreUp;
+    public AudioClip damage;
     
     // Start is called before the first frame update
     void Start()
@@ -30,8 +32,26 @@ public class Player : MonoBehaviour
         // Mobile
         Vector2 touch = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10f));
         if (Input.touchCount > 0) {
-            transform.position = new Vector3(touch.x, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(touch.x, transform.position.y, transform.position.z);
+
+            transform.position = Vector3.Lerp(gameObject.transform.position,
+            new Vector3(Mathf.Clamp(gameObject.transform.position.x + touch.x, -2.5f, 2.5f),
+            gameObject.transform.position.y, gameObject.transform.position.z), directionalSpeed * Time.deltaTime);
         }
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "scoreup")
+        {
+            GetComponent<AudioSource>().PlayOneShot(scoreUp, 1.0f);
+        }
+
+        if (other.gameObject.tag == "triangle")
+        {
+            GetComponent<AudioSource>().PlayOneShot(damage, 1.0f);
+        }
+    }
+
 }
