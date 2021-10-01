84a1dba07a8651e52c26a1024629b1ca7a664b23
Zachary M Thomas <zach711595@hotmail.com>
2021-10-01 17:30:39 -0400

    added persistent high score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,12 +6,25 @@ using TMPro;
 public class Score : MonoBehaviour
 {
     public int score;
+    public int highScore;
     public TextMeshProUGUI scoreUI;
+    public TextMeshProUGUI highScoreUI;
+
+    private void Start()
+    {
+        highScore = PlayerPrefs.GetInt("highScore");
+    }
 
     // Update is called once per frame
     void Update()
     {
         scoreUI.text = score.ToString();
+        highScoreUI.text = highScore.ToString();
+        if(score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("highScore", highScore);
+        }
     }
 
     void OnTriggerEnter(Collider other)
